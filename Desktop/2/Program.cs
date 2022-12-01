00bc7fb339490125eed02653a30c21374fe4b559
dinur <dinur00909@yandex.ru>
2022-12-01 21:40:29 +0300

    Programm finished

---
+++ Desktop/2/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    if (a>c)
+    {
+        Console.WriteLine("Максимальное число - первое:");
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число - третье:");
+        Console.WriteLine(c);
+    }
+}
+else if (b>c)
+{
+    Console.WriteLine("Максимальное число - второе:");
+    Console.WriteLine(b);
+}
+else
+{
+    Console.WriteLine("Максимальное число - третье:");
+    Console.WriteLine(c);
+}
