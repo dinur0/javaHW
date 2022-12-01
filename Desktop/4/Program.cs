060e8c1ad327ae74d027dc7f05726e460311a761
dinur <dinur00909@yandex.ru>
2022-12-01 21:13:42 +0300

    Programm finished

---
+++ Desktop/4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+while (res < a-1)
+{
+    res = res+2;
+    Console.Write(res);
+    Console.Write(",");
+}
