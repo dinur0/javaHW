65ac80543ae040b4a23379335e8542a38b35a5c5
dinur <dinur00909@yandex.ru>
2022-12-07 18:16:24 +0300

    programm finished

---
+++ Desktop/Новая папка/333/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+while (index <= x)
+{
+    Console.WriteLine(Math.Pow(index,3));
+    index++;
+}
+
