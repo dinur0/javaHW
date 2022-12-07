35707efdcbbc7cde1360e07fb097d234f0433075
dinur <dinur00909@yandex.ru>
2022-12-07 18:19:58 +0300

    file location changed

---
+++ Desktop/Новая папка/111/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите 5-значное число");
+string array = Convert.ToString(Console.ReadLine());
+if (array.Length == 5)
+{
+    if (array[0]==array[4] && array[1]==array[3])
+    {
+        Console.WriteLine("Число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число НЕ является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine("Число НЕ пятизначное");
+}
