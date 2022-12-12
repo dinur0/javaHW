761db8eaad773e4a035b220d7dfe3245e81bfbaa
dinur <dinur00909@yandex.ru>
2022-12-12 18:37:23 +0300

    homework complited

---
+++ Desktop/папка/2/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ответ: {method(a)}");
+
+
+
+
+int method(int number)
+{
+    string strnum = Convert.ToString(number);
+    int[] b = new int[strnum.Length];
+    int sum = 0;
+    for (int i = 0; i < strnum.Length; i++)
+    {
+        b[i] = Convert.ToInt32(strnum[i].ToString());
+        sum += b[i];
+    }
+    return sum;
+}
