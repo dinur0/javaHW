0ab584ac0a0854711c9f1883005059acb7789179
dinur <dinur00909@yandex.ru>
2022-12-13 17:45:45 +0300

    programm finished

---
+++ Desktop/папка/1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ответ:{method(a,b)}");
+
+int method(int number1, int number2)
+{
+    int sum = number1;
+    for (int i = 0; i < number2-1; i++)
+    {
+      sum *= a;
+    }
+    return sum;
+}
