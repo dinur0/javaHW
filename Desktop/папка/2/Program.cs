3bd78aaab36dd2674093e31efef8bf12a01b4e54
dinur <dinur00909@yandex.ru>
2022-12-13 17:38:58 +0300

    homework complited

--- Desktop/папка/2/Program.cs
+++ Desktop/папка/2/Program.cs
@@ -7,12 +7,12 @@ Console.WriteLine($"Ответ: {method(a)}");
 
 int method(int number)
 {
-    string strnum = Convert.ToString(number);
-    int[] b = new int[strnum.Length];
+    string strmun = Convert.ToString(number);
+    int[] b = new int[strmun.Length];
     int sum = 0;
-    for (int i = 0; i < strnum.Length; i++)
+    for (int i = 0; i < strmun.Length; i++)
     {
-        b[i] = Convert.ToInt32(strnum[i].ToString());
+        b[i] = Convert.ToInt32(strmun[i].ToString());
         sum += b[i];
     }
     return sum;
