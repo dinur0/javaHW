0a2de543e8cfd93500c61571b884aa71324f4ff0
dinur <dinur00909@yandex.ru>
2022-12-15 14:56:12 +0300

    programm finished

---
+++ Desktop/Новая папка/3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ответ: {method(a)}");
+
+double method(int size)
+{
+    double[] array = new double[size];
+    // int max = 0;
+    // int min = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i]=Math.Round(new Random().Next(1, 10)+new Random().NextDouble(),3);
+        
+    }
+
+    Console.WriteLine(string.Join("|",array));
+    Console.WriteLine($"Макс: {array.Max()}");
+    Console.WriteLine($"Мин: {array.Min()}");
+    return (array.Max()-array.Min());
+}
