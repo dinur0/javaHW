56e37c2d715e3ccb693bfcc0cacd5bcc6cbb5b70
dinur <dinur00909@yandex.ru>
2022-12-15 14:58:24 +0300

    programm finished

---
+++ Desktop/Новая папка/1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите размер массива: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ответ: {method(a)}");
+
+int method(int size)
+{
+    int[] array = new int[size];
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        array[i]=new Random().Next(100, 1000);
+        if (array[i]%2==0)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine(string.Join(",",array));
+    return count;
+}
