2f63951517ec786e0732697c8ed776ba32b26f5a
dinur <dinur00909@yandex.ru>
2022-12-22 19:41:22 +0300

    deleted comments

--- Desktop/Новая папка/1/Program.cs
+++ Desktop/Новая папка/1/Program.cs
@@ -41,19 +41,3 @@ double[,] FillArray(int line, int col)
     return array;
 }
 
-
-
-// double[,] FillArray(int first, int second)
-// {
-// double[,] array = new double[first, second];
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             array[i, j]= Math.Round(new Random().Next(-10,11)+ new Random().NextDouble(),1);
-//             Console.Write(array[i,j]+"|");
-//         }
-//         Console.WriteLine(array);
-//     }
-//     return array;
-// }
