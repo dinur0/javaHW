fb14d88e5027d815017e698bf17818fdeb0fdefc
dinur <dinur00909@yandex.ru>
2022-12-22 19:43:22 +0300

    deleted comments

--- Desktop/Новая папка/1/Program.cs
+++ Desktop/Новая папка/1/Program.cs
@@ -1,26 +1,4 @@
-﻿// Console.WriteLine("Введите количество цифр: ");
-// int size = Convert.Toint32(Console.ReadLine());
-// Console.WriteLine(String.Join(",",createArrayAndCount(size)));
-
-// int[] createArrayAndCount(int num)
-// {
-//     int[] array = new int [num];
-//     int count = 0;
-// for (int i = 0; i < num; i++)
-// {
-//     Console.WriteLine($"Введите число №{i+1}: ");
-//     array[i] = Convert.Toint32(Console.ReadLine());
-//     if (array[i]>0) // 0-положительное число?
-//     {
-//         count++;
-//     }
-// }
-// Console.WriteLine(String.Join(",",array));
-// Console.WriteLine($"Положительных чисел: {count}");
-// return array;
-// }
-
-Console.WriteLine("Введите кол-во строк: ");
+﻿Console.WriteLine("Введите кол-во строк: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите кол-во столбцов: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
