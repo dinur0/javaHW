d4653d3876fc5892ba144f87a4b8f672e1292ee2
dinur <dinur00909@yandex.ru>
2022-12-22 19:39:24 +0300

    new programm

--- Desktop/Новая папка/1/Program.cs
+++ Desktop/Новая папка/1/Program.cs
@@ -1,21 +1,59 @@
-﻿Console.WriteLine("Введите количество цифр: ");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(String.Join(",",createArrayAndCount(size)));
+﻿// Console.WriteLine("Введите количество цифр: ");
+// int size = Convert.Toint32(Console.ReadLine());
+// Console.WriteLine(String.Join(",",createArrayAndCount(size)));
 
-int[] createArrayAndCount(int num)
+// int[] createArrayAndCount(int num)
+// {
+//     int[] array = new int [num];
+//     int count = 0;
+// for (int i = 0; i < num; i++)
+// {
+//     Console.WriteLine($"Введите число №{i+1}: ");
+//     array[i] = Convert.Toint32(Console.ReadLine());
+//     if (array[i]>0) // 0-положительное число?
+//     {
+//         count++;
+//     }
+// }
+// Console.WriteLine(String.Join(",",array));
+// Console.WriteLine($"Положительных чисел: {count}");
+// return array;
+// }
+
+Console.WriteLine("Введите кол-во строк: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+FillArray(num1, num2);
+
+double[,] FillArray(int line, int col)
 {
-    int[] array = new int [num];
-    int count = 0;
-for (int i = 0; i < num; i++)
-{
-    Console.WriteLine($"Введите число №{i+1}: ");
-    array[i] = Convert.ToInt32(Console.ReadLine());
-    if (array[i]>0) // 0-положительное число?
+    double[,] array = new double[line, col];
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        count++;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(new Random().Next(-10, 11) + new Random().NextDouble(), 1);
+            Console.Write(array[i, j] + "|");
+        }
+        Console.WriteLine();
     }
+    return array;
 }
-Console.WriteLine(String.Join(",",array));
-Console.WriteLine($"Положительных чисел: {count}");
-return array;
-}
+
+
+
+// double[,] FillArray(int first, int second)
+// {
+// double[,] array = new double[first, second];
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             array[i, j]= Math.Round(new Random().Next(-10,11)+ new Random().NextDouble(),1);
+//             Console.Write(array[i,j]+"|");
+//         }
+//         Console.WriteLine(array);
+//     }
+//     return array;
+// }
