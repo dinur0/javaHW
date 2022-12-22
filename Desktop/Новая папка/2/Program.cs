eef5b4eb77edc161e46a54e3d47c34e683dd0d55
dinur <dinur00909@yandex.ru>
2022-12-22 19:35:58 +0300

    new programm

--- Desktop/Новая папка/2/Program.cs
+++ Desktop/Новая папка/2/Program.cs
@@ -1,17 +1,36 @@
-﻿Console.WriteLine("Введите первую координату первой линии: ");
-double b1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите вторую координату первой линии: ");
-double k1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите превую координату второй линии: ");
-double b2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите вторую координату второй линии: ");
-double k2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Ответ: {String.Join("|",pointSearch(b1, k1, b2, k2))}");
-double[] pointSearch(double numb1, double numk1, double numb2, double numk2)
+﻿Console.WriteLine("Введите кол-во строк: ");
+int line = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер строки: ");
+int IndexLine = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int IndexCol = Convert.ToInt32(Console.ReadLine());
+ShowNumber(FillArray(line, col), IndexLine, IndexCol);
+
+int[,] FillArray(int first, int second)
 {
-    double x = (numb2-numb1) / (numk1-numk2);
-    double[] array = new double [2];
-    array[0] = numk1*x+numb1;
-    array[1] = numk2*x+numb2;
+    int[,] array = new int[first, second];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-10, 11);
+            Console.Write(array[i, j] + "|");
+        }
+        Console.WriteLine();
+    }
     return array;
+}
+
+void ShowNumber(int[,] arr, int num1, int num2)
+{
+    if (num1 <= arr.GetLength(0) || num2 <= arr.GetLength(1))
+    {
+       Console.WriteLine($"Число: {arr[num1-1, num2-1]}"); //добавил (- 1)
+    }
+    else
+    {
+    Console.WriteLine("Такого числа нет");
+    }
 }
