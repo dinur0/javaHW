92e9dd76b8e3c838a19c6717146996b4654f1984
dinur <dinur00909@yandex.ru>
2022-12-26 18:53:22 +0300

    finished

--- Desktop/Новая папка/2/Program.cs
+++ Desktop/Новая папка/2/Program.cs
@@ -1,36 +1,18 @@
-﻿Console.WriteLine("Введите кол-во строк: ");
-int line = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите кол-во столбцов: ");
-int col = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер строки: ");
-int IndexLine = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите номер столбца: ");
-int IndexCol = Convert.ToInt32(Console.ReadLine());
-ShowNumber(FillArray(line, col), IndexLine, IndexCol);
-
-int[,] FillArray(int first, int second)
+﻿int SumNumbers(int number1, int number2)
 {
-    int[,] array = new int[first, second];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(-10, 11);
-            Console.Write(array[i, j] + "|");
-        }
-        Console.WriteLine();
-    }
-    return array;
+if (number1<number2)
+{
+return SumNumbers(number1+1, number2)+number1;
+}
+else if (number1>number2)
+{
+return SumNumbers(number1-1, number2)+number1;
+}
+else return number1;
 }
 
-void ShowNumber(int[,] arr, int num1, int num2)
-{
-    if (num1 <= arr.GetLength(0) || num2 <= arr.GetLength(1))
-    {
-       Console.WriteLine($"Число: {arr[num1-1, num2-1]}"); //добавил (- 1)
-    }
-    else
-    {
-    Console.WriteLine("Такого числа нет");
-    }
-}
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumNumbers(num1, num2));
