2040f699fb32c239374481fb1bd6f9b04dc41d0d
dinur <dinur00909@yandex.ru>
2022-12-26 18:54:19 +0300

    finished

--- Desktop/Новая папка/3/Program.cs
+++ Desktop/Новая папка/3/Program.cs
@@ -1,36 +1,13 @@
-﻿Console.WriteLine("Введите кол-во строк: ");
+﻿int Akkerman(int m, int n)
+{
+if (m==0) return n+1;
+else if (m>0 && n==0) return Akkerman(m-1,1);
+else if (m>0 && n>0) return Akkerman(m-1, Akkerman(m,n-1));
+return 0;
+}
+
+Console.WriteLine("Введите первое число: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите кол-во столбцов: ");
+Console.WriteLine("Введите второе число: ");
 int num2 = Convert.ToInt32(Console.ReadLine());
-ArithmeticMean(FillArray(num1, num2));
-
-double[,] FillArray(int line, int col)
-{
-    double[,] array = new double[line, col];
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-            Console.Write(array[i, j] + "|");
-        }
-        Console.WriteLine();
-    }
-    return array;
-}
-
-void ArithmeticMean(double[,] arr)
-{
-    Console.Write($"Ответ: ");
-    for (int j = 0; j < arr.GetLength(1); j++)
-    {
-        double score = 0;
-        double temp = 0;
-        for (int i = 0; i < arr.GetLength(0); i++)
-        {
-            temp += arr[i,j];
-            score = Math.Round(temp/arr.GetLength(0),2);
-        }
-        Console.Write(score+"|");
-    }
-}
+Console.WriteLine(Akkerman(num1, num2));
