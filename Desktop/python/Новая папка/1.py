86cb9e184aa4e11c1cd90849444505ac94aafe29
аывпфывпирывар <sxfgbvszdfb>
2023-03-04 16:22:04 +0300

    complete lession 4

--- Desktop/python/Новая папка/1.py
+++ Desktop/python/Новая папка/1.py
@@ -1,11 +1,16 @@
-import random
-n = int(input("Введите кол-во элементов: "))
-array = []
-x = int(input("Введите число: "))
-count = 0
-for i in range(n):
-    array.append(int(input("Введите элемент массива: ")))
-    if array[i]==x:
-        count+=1
-print(*array)
-print(f"Ответ: {count}")
+def fillArray(number):
+    array = []
+    for j in range(number):
+        array.append(int(input("Введите элемент массива: ")))
+    return array
+
+a = int(input("Введите кол-во первого множества: "))
+list1 = fillArray(a)
+b = int(input("Введите кол-во второго множества: "))
+list2 = fillArray(b)
+result = []
+for i in list1:
+    if i in list2:
+        result.append(i)
+list.sort(result)
+print(*result)
--- Desktop/python/Новая папка/2.py
+++ Desktop/python/Новая папка/2.py
@@ -2,17 +2,11 @@ import random
 n = int(input("Введите кол-во элементов: "))
 list = []
 for i in range(n):
-    list.append(int(input("Введите элемент массива: ")))
-x = int(input("Введите число: "))
-diffList = []
-diffList.extend(list)
-for y in range(n):
-    diffList[y]=x-list[y]
-    if diffList[y]<0:
-        diffList[y]*=-1
-print(list[diffList.index(min(diffList))])
-print(*list)
-
-
-
-
+    list.append(random.randint(1,10))
+count = list[-2]+list[-1]+list[0]
+for i in range(len(list)-1):
+    if count<list[i-1]+list[i]+list[i+1]:
+        count=list[i-1]+list[i]+list[i+1]       
+print(list)
+print(count)
+    
