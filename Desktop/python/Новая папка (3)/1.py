7d11e5bb5646d6748f20673f31c1df6be39afc02
аывпфывпирывар <sxfgbvszdfb>
2023-03-15 19:53:25 +0300

    complete

--- Desktop/python/Новая папка (3)/1.py
+++ Desktop/python/Новая папка (3)/1.py
@@ -1,6 +1,17 @@
-a = int(input("Введите первое число: "))
-b = int(input("Введите разность: "))
-c = int(input("Введите кол-во: "))
-for i in range(c):
-    print(a,end = " ")
-    a+=b
+letters = ["а", "я", "у", "ю", "о", "е", "ё", "э", "и", "ы"]
+
+def countLetters(x):
+    count=0
+    for i in x:
+        if i in letters:
+            count+=1
+    return count
+def same(р):
+    return len(set(р)) <= 1
+
+b = input().split()
+c = list(map(countLetters,b))
+if same(c)==True:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
--- Desktop/python/Новая папка (3)/2.py
+++ Desktop/python/Новая папка (3)/2.py
@@ -1,25 +1,7 @@
-def fillArray(number):
-    array = []
-    for j in range(number):
-        array.append(int(input("Введите элемент массива: ")))
-    return array
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1,num_rows+1):
+        for j in range(1,num_columns+1):
+            print (operation(i,j), end = " ")
+        print()
 
-def indexAdd(abr):
-    counts = []
-    for i in range(len(abr)):
-        counts.append([i,abr[i]])
-    return counts
-
-    
-num = int(input("Введите кол-во: "))
-list = fillArray(num)
-a = int(input("Введите минимальное число: "))
-b = int(input("Введите максимальное число: "))
-counts = []
-for i in range(len(list)):
-    counts.append([i,list[i]])
-print(list)
-for i in counts:
-    if a<=i[1]<=b:
-        print(i[0], end=" ")
-
+print_operation_table(lambda x,y: x*y)
