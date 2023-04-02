95ad329a52bfc19a2eb20821d4c93cad725fed67
dinur0 <119072459+dinur0@users.noreply.github.com>
2023-04-02 19:14:06 +0300

    Update programm3.java

--- Desktop/java/programm3.java
+++ Desktop/java/programm3.java
@@ -2,24 +2,31 @@ import java.util.Scanner;
 
 public class programm3 {
     public static void main(String[] args) {   
-        Scanner iScanner = new Scanner(System.in);
-        System.out.println("выберите функцию: \n1.+\n2.- \n3.* \n4./ ");
-        int choice = iScanner.nextInt();
-        System.out.println("x:");
-        int x = iScanner.nextInt();
-        System.out.println("y:");
-        int y = iScanner.nextInt();
-        if (choice==1) {
-            System.out.println(x+y);
-        } else if (choice==2) {
-                System.out.println(x-y);
-            } else if (choice==3) {
-                System.out.println(x*y);
-                } else if (choice==4) {
-                System.out.println(x/y);
-                } else{
-                    System.out.println("неверная команда");
-                }
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("кол-во строк");
+        int n = sc.nextInt();
+        String[] recepts = new String[n];
+        for (int num = 0; num < n; num++) {
+            recepts[num]=sc.next();
+        }
+        System.out.println("кол-во продуктов");
+        int m = sc.nextInt();
+        String[] products = new String[m];
+        for (int r = 0; r < m; r++) {
+            products[r]=sc.next();
+        }
+        for (int i = 0; i < recepts.length; i++) {
+            String [] receptSplit = recepts[i].split("");
+            for (int j = 0; j < receptSplit.length; j++) {
+                for (int k = 0; k < products.length; k++) {
+                    String [] productSplit = products[k].split("[-]");
+                    if (receptSplit[j].equals(productsSplit[0])){
+                        receptSplit[j]=productsSplit[1];
+                    }
+                    
+                } 
+            }
+        System.out.print(String.join("", receptSplit));
+        }
         }
     }
